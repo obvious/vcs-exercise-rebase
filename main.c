92f35f469a845e598a5ce1dd4cd8fe88ba85db99
Varun Barad <varun@varunbarad.com>
2023-03-07 00:41:44 +0530

    Flesh out C program

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+const char *get_name() {
+    static char buf[128];
+    scanf("%s", buf);
+    return buf;
+}
+
 int main(int argc, char *argv[]) {
+    printf("What's your name? ");
+    const char *name = get_name();
+    printf("Hello, %s!\n", name);
     return 0;
 }
